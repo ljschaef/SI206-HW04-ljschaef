ab2c15131636d5ca36cd90d0fc6883be7538c3d8
Lucas Schaefer <lucasschaefer@0587334015.wireless.umich.net>
2018-01-31 13:36:12 -0500

    empty

--- magic_eight.py
+++ magic_eight.py
@@ -1,10 +1,4 @@
-import randrange from random
-
-
-def magic_eight(question):
-    if question[len(question) - 1] != '?':
-        print("I'm sorry, I only accept questions")
-    else:
-        rand_answer = randrange(0, 20)
-        return rand_answer
+def magic_eight_ball():
+    response = input("What is your question?")
+    return response
 
