ed8c92911e63533eb4696b8957e98166841896b5
Alex <tritonytrades@gmail.com>
2018-01-31 14:48:11 -0500

    final product

--- magic_eight.py
+++ magic_eight.py
@@ -1,35 +1,12 @@
-<<<<<<< HEAD
+
 import random
 
 
-
-
-
-=======
 def magic_eight_ball():
-    response = input("What is your question?")
-    return response
->>>>>>> de2d8eee62e85fdc10f601f011f8b3f27649b0e9
+ response = input("What is your question? ")
+ answers =["It is certain","It is decidedly so","Without a doubt","Yes definitely","You may rely on it","As I see it, yes","Most likely","Outlook good","Yes","Signs point to yes","Reply hazy try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"]
+ get_answer = random.choice(answers)
+ print(get_answer)
 
-answers = ["It is certain",
-"It is decidedly so",
-"Without a doubt",
-"Yes definitely",
-"You may rely on it",
-"As I see it, yes",
-"Most likely",
-"Outlook good",
-"Yes",
-"Signs point to yes",
-"Reply hazy try again",
-"Ask again later",
-"Better not tell you now",
-"Cannot predict now",
-"Concentrate and ask again",
-"Don't count on it",
-"My reply is no",
-"My sources say no",
-"Outlook not so good"
-"Very doubtful"]
 
-get_answer = random.choice(answers)
+magic_eight_ball()
