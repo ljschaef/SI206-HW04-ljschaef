aecd25cece91b0c2bc5335dd296d9ace5731047d
Lucas Schaefer <lucasschaefer@0587334015.wireless.umich.net>
2018-01-31 13:51:21 -0500

    prolly fucked it up

--- magic_eight.py
+++ magic_eight.py
@@ -2,3 +2,18 @@ def magic_eight_ball():
     response = input("What is your question?")
     return response
 
+def check_question():
+    response = magic_eight_ball()
+    if response[len(response) - 1] != '?':
+        print("I'm sorry, I can only accept questions")
+        return 1
+    elif response == "quit":
+        return 0
+
+def __main__():
+    stuff = check_question()
+    while stuff != 0:
+        check_question()
+        pass
+    print("Thanks for playing!")
+    pass
